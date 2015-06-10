3f540f933ee5e91058fc7e5cca7cb0a348de4cd3
Simon Krajewski <simon@haxe.org>
2015-06-10 22:04:28 +0200

    add Null to `follow_once`

--- interp.ml
+++ interp.ml
@@ -2543,6 +2543,8 @@ let macro_lib =
 					(match !r with
 					| None -> t
 					| Some t -> t)
+				| TAbstract({a_path = [],"Null"},[t]) ->
+					t
 				| TAbstract _ | TEnum _ | TInst _ | TFun _ | TAnon _ | TDynamic _ ->
 					t
 				| TType (t,tl) ->
